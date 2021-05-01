b84fa5a2fa74f4cd34926f67242e63ac3e0a9b35
jolayemi_local <ajolayemi82@gmail.com>
2021-05-01 22:35:30 +0200

    Completed stage 3

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
 if __name__ == '__main__':
-    expr = input().split()
-    print(eval(f'{expr[0]} + {expr[1]}'))
+    while True:
+        expr = input()
+        if expr == '/exit':
+            print('Bye!')
+            break
+        elif expr == '/help':
+            print('A simple calculator that only adds values as at now')
+
+        elif expr == '':
+            continue
+
+        elif len(expr.split()) == 1:
+            print(expr)
+
+        else:
+            split_expr = list(map(lambda x: int(x), expr.split()))
+            print(sum(split_expr))
